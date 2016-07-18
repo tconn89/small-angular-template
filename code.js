15671fb0c07b2665a6beabfb996528f62661da19
Ty Connors <tconn89@gmail.com>
2016-07-17 22:22:59 -0400

    add basic functionality to ng-app myApp

--- code.js
+++ code.js
@@ -1,7 +1,15 @@
 
-angular.module('myApp',['ngMaterial'])
+InboxCtrl = function InboxCtrl($scope) {
+      re = /[5-8]/
+      $scope.title = 'Write a title here...' ;
 
-
+      $scope.titleFilter = function() {
+        $scope.title = $scope.title.match(re)
+        console.log("hey")
+      }
+};
+angular.module('myApp',['ngMaterial']).controller('InboxCtrl',['$scope', InboxCtrl])
+  
 /*
 Copyright 2016 Google Inc. All Rights Reserved. 
 Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
